3ab8244062e18cb2e86de7049301280467d915fc
Frank <Frankmorasanchez@gmail.com>
2023-05-03 18:13:03 -0600

    ejercicio open boootcamp

---
+++ node_project_first/Frank_miInfo.js
@@ -0,0 +1,12 @@
+const miInfo={
+    nombre:"Frank",
+    Edad: 22,
+    "Eres-desarrollador?":true,
+    "Fecha_de _nacimiento":new Date("2000-03-30"),
+    LibroFavorito:{
+        titulo:"KING CORP: EL IMPERIO NUNCA CONTADO DE JUAN CARLOS I",
+        autor:"jose mario olmo",
+        fecha:new Date(2023-05-08),
+        url:"https://www.casadellibro.com/libro-king-corp-el-imperio-nunca-contado-de-juan-carlos-i/9788419119308/13634668"
+    }
+};
